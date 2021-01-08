511ed01518fcd9029dbd5069824bb1a87729a57b
DESKTOP-ABSCGQ6\Tom <tomkerrdev@gmail.com>
2021-01-08 13:46:46 +0000

    Added MA

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ import random
 import sc2
 from sc2.ids.ability_id import AbilityId as AbilID
 from sc2.ids.unit_typeid import UnitTypeId as UnitID
+from MapAnalyzer import MapData
 
 
 class RoachRush(sc2.BotAI):
